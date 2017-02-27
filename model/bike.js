67d1658bd2535c3966e17ebbe702d824b8dc288b
Dana Kulp <dkulp23@gmail.com>
2017-02-27 14:28:12 -0800

    Created constructor for bike objects with id, brand and type properties.

---
+++ model/bike.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const uuid = require('node-uuid');
+const debug = require('debug')('bike:bike');
+const createError = require('http-errors');
+const storage = require(`${__dirname}/../lib/storage.js`);
+
+const Bike = module.exports = function(brand, type) {
+  debug('bike constructor');
+
+  if (!brand) throw createError(400, 'bike brand expected');
+  if (!type) throw createError(400, 'bike type expected');
+
+  this.id = uuid.v1();
+  this.brand = brand;
+  this.type = type;
+};
+
+Bike.createBike = function(_bike) {
+  debug('createBike');
+
+  try {
+    let bike = new Bike(_bike.brand, _bike.type);
+    return storage.createItem(bike);
+  } catch(err) {
+    return Promise.reject(err);
+  }
+};
+
+Bike.fetchBike = function(id) {
+  debug('fetchBike');
+  return storage.fetchItem('bike', id);
+};
