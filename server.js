c4bc9116b0cbe597f685ea5edace0b12a8009181
Dana Kulp <dkulp23@gmail.com>
2017-02-27 13:26:05 -0800

    Create simple server using express. Set up debug for testing.

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const debug = require('debug')('bike:server');
+const app = express();
+
+app.listen(PORT, function() {
+  debug(`Server's up on port: ${PORT}`);
+});
